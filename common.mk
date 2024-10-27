fbeadef13ba6ab1faa9d60089a1679ad14829040
Nahhk <nahhk@proton.me>
2024-10-27 19:04:08 +0000

    sm6225-common: Adapt to PixelOS
    Change-Id: I373c471388bb1a24e3007fe4adee8862e2e77d3c

--- common.mk
+++ common.mk
@@ -435,9 +435,9 @@ PRODUCT_PACKAGES += \
     TelephonyResCommon
 endif
 
-# Overlays Lineage
+
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay-custom
 
 # Partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
